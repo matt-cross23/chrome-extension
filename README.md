b9d88e5ed1da2c3040084368b80ffc1fd30814ee
Matthew Cross <matthewcross1017@gmail.com>
2022-07-13 16:12:11 -0400

    add tp background

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chrome-extension
+
+[Probably going to have use android TTS](https://developer.android.com/reference/kotlin/android/speech/tts/TextToSpeech)
--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 // background.js
+// Script that is always running in background
+// Do not put event listeners or code that depends on the page\
 
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
+  // Mkae sure script is in background
+  console.log("background script connected")
+
+  // Old code goes into storage and grabs color
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
+  
+  // Get whole list of voices 
+  chrome.tts.getVoices(function(availableVoices) {
+    voices = availableVoices;
+    console.log(voices)
+  });
 });
 
 
